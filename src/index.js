184c5a8e9d6ca4d772c63ec34a4d5f1b94056204
Prateek Kumar Jha <prateekj1211@gmail.com>
2024-04-05 00:03:09 +0530

    added webpack config for image, fonts and css

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-import { content } from "./home";
-alert("Hello, Prateek!");
-console.log(content, "container")
+import { content } from "./pages/home.js";
+// alert("Hello, Prateek!");
+console.log(content, "container");
+// webpack : handle fonts, proper error message
+
+// const content = document.querySelector("#content");
+// content.innerHTML = `<div>Hello World</div>`;
+console.log(content);
